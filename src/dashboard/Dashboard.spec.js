047af1abf283b7bd450710c927a7a4229848aee3
Katrina Roaix <kroaix@gmail.com>
2019-11-13 04:33:22 -0500

    MVP Complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
-// Test away
+import React from 'react';
+import { render } from "@testing-library/react";
+import Dashboard from './Dashboard';
+
+test("Dashboard shows the controls and display", () => {
+  const { getByText } = render(<Dashboard />);
+
+  getByText(/Unlocked/i);
+  const display = getByText(/Open/i);
+  getByText(/Lock Gate/i);
+  const controls = getByText(/Close Gate/i);
+
+  expect(display).toBeDefined;
+  expect(controls).toBeDefined;
+});
